8645f3cac7cb8b4c8e9e094f56b35ae90c389219
sasakama2 <karage350@gmail.com>
2025-12-07 19:49:54 +0900

    edit test code

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -1,6 +1,7 @@
 # db_manager.py が同じディレクトリにあることを想定
 from DB_manager import DBManager 
 import pathlib
+import time 
 
 SCRIPT_DIR = pathlib.Path(__file__).parent.absolute()
 
@@ -9,27 +10,22 @@ DB_PATH = SCRIPT_DIR.parent.parent / 'db' / 'static_data.db'
 
 # DBManagerのインスタンス化
 db_manager = DBManager(db_name=str(DB_PATH))
-
-# 3. データを挿入
+current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+print(f"[{current_time}]")
 type = "通常"
 effects = [
-    2,
+    0,
     9,
     4,
 ]
 demerits = []
 color = "赤"
+try:
+    db_manager.insert_relic(relic_type=type, effects=effects, demerits=demerits, color=color)
+    print(f"[{current_time}] Inserted relic of type {type} with effects {effects} and color {color}.")
+except Exception as e:
+    print(f"[{current_time}] Error inserting relic: {e}")
 
-db_manager.insert_relic(relic_type=type, effects=effects, demerits=demerits, color=color)
-
-type = "深層"
-effects = [
-    8,
-    4,
-]
-demerits = [6]
-color = "青"
-db_manager.insert_relic(relic_type=type, effects=effects, demerits=demerits, color=color)
 
 # 6. 接続を閉じる
 db_manager.close()
--- tests/test_DBmanager.py
+++ tests/test_DBmanager.py
@@ -9,8 +9,12 @@ DB_PATH = SCRIPT_DIR.parent / 'db' / 'static_data.db'
 # DBManagerのインスタンス化
 db_manager = DBManager(db_name=str(DB_PATH))
 
+def test_init_tables():
+    db_manager._connect()
+    db_manager.init_tables()
 
 def test_insert_relic():
+    db_manager._connect()
     type = "通常"
     effects = [
         2,
@@ -32,24 +36,24 @@ def test_insert_relic():
     db_manager.insert_relic(relic_type=type, effects=effects, demerits=demerits, color=color)
 
 def test_fetch_vessels():
+    db_manager._connect()
     for character_id in range(1,10):
         vessels = db_manager.fetch_character_vessels(character_id)
         assert isinstance(vessels, list), "Fetched vessels should be a list."
         for vessel in vessels:
-            assert 'vessel_id' in vessel and 'vessel_name' in vessel, "Vessel should have 'vessel_id' and 'vessel_name'."
+            print(vessel)
 
-def test_init_tables():
-    db_manager._connect()
-    db_manager.init_tables()
 
 def test_fetch_relics_by_effect():
+    db_manager._connect()
     id = 4
     relics = db_manager.fetch_relics_by_effect(effect_id=id)
     assert isinstance(relics, list), "Fetched relics should be a list."
     for relic in relics:
-        assert 'relic_id' in relic and 'relic_type' in relic, "Relic should have 'relic_id' and 'relic_type'."
+        assert 'relic_id' in relic[0] and 'relic_type' in relic[1] and 'color' in relic[2], "Relic dictionary should contain relic_id, relic_type, and color."
 
 def test_search_effect_by_term():
+    db_manager._connect()
     term = "上昇"
     effects = db_manager.search_effect_by_term(term)
     assert isinstance(effects, list), "Searched effects should be a list."
